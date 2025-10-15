d09d404af7a6e8db4433326efaadcb77d162bcd1
YBQuintus <yourboiquintus@gmail.com>
2025-10-15 12:19:45 +0100

    Fix InteractionService DI

--- src/Program.cs
+++ src/Program.cs
@@ -9,7 +9,12 @@ using IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddSingleton<DiscordSocketClient>();       // Add the discord client to services
-        services.AddSingleton<InteractionService>();        // Add the interaction service to services
+        services.AddSingleton<InteractionService>(provider =>
+        {
+            var client = provider.GetRequiredService<DiscordSocketClient>();
+            return new InteractionService(client);
+        });
+        // Add the interaction service to services
         services.AddHostedService<InteractionHandlingService>();    // Add the slash command handler
         services.AddHostedService<DiscordStartupService>();         // Add the discord startup service
     })
